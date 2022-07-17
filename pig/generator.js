d3acdb6eacb61f58a68c5a50300533c4c1ae5632
Ricardo <ricardovelasquez681@gmail.com>
2022-07-16 21:00:05 -0700

    Created a working Project Idea Generator

---
+++ pig/generator.js
@@ -0,0 +1,33 @@
+
+const ideas = new Map([
+    // Key, Value#####
+    // In This Case I Am Making The Key An Int So I Can Specify A Value Using Randomization#####
+    [0, "Calculator"],
+    [1, "Portfolio"],
+    [2, "Resume"],
+    [3, "Tribute Page"],
+    [4, "Survey"],
+    [5, "Resturant Website"],
+    [6, "Photography Website"],
+    [7, "Porduct Display Website"],
+    [8, "Technical Documentation"],
+    [9, "Blog"],
+    [10, "Journal"],
+    [11, "Dictionary"]
+
+]);
+
+function generate(){
+    let ideaEle = document.getElementById("idea");
+    // Gets The Child ELEMENT Of The Parent Element#####
+    let child = ideaEle.firstElementChild;
+
+    child.innerHTML = ideas.get(randInt(ideas.size));
+
+}
+
+// The Max Int Is Excluded#####
+function randInt(max){
+    return Math.floor(Math.random() * max);
+
+}
