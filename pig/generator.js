6d2913c6e8a83dcf00b4af36734e7574239e49f9
Ricardo <ricardovelasquez681@gmail.com>
2022-07-17 21:36:01 -0700

    Changed map list of ideas into an array for PIG, also added more ideas

--- pig/generator.js
+++ pig/generator.js
@@ -1,28 +1,29 @@
 
-const ideas = new Map([
-    // Key, Value#####
-    // In This Case I Am Making The Key An Int So I Can Specify A Value Using Randomization#####
-    [0, "Calculator"],
-    [1, "Portfolio"],
-    [2, "Resume"],
-    [3, "Tribute Page"],
-    [4, "Survey"],
-    [5, "Restaurant Website"],
-    [6, "Photography Website"],
-    [7, "Product Display Website"],
-    [8, "Technical Documentation"],
-    [9, "Blog"],
-    [10, "Journal"],
-    [11, "Dictionary"]
+const ideas = [
+    "Calculator",
+    "Portfolio",
+    "Resume",
+    "Tribute Page",
+    "Survey",
+    "Restaurant Website",
+    "Photography Website",
+    "Product Display Website",
+    "Technical Documentation",
+    "Blog",
+    "Journal",
+    "Dictionary",
+    "Animation",
+    "Cards",
+    "Instagram Post",
 
-]);
+];
 
 function generate(){
     let ideaEle = document.getElementById("idea");
     // Gets The Child ELEMENT Of The Parent Element#####
     let child = ideaEle.firstElementChild;
 
-    child.innerHTML = ideas.get(randInt(ideas.size));
+    child.innerHTML = ideas[randInt(ideas.length)];
 
 }
 
